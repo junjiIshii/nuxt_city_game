e0597c07da9f9343e2805a58ccb1764742362313
石井惇仁 <>
2020-03-15 16:04:30 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <h1>INDEX</h1>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+
+</style>
