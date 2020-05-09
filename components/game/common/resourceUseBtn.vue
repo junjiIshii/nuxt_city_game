f64207acba7d7020f6263db41d3a3d4c7d139660
junji-media <ishii.mediach@gmail.com>
2020-05-10 00:44:49 +0900

    レベルアップに必要な資源で、足りる場合、足りない場合に色が変わるクラスを付与するcomputedを追加

--- components/game/common/resourceUseBtn.vue
+++ components/game/common/resourceUseBtn.vue
@@ -1,22 +1,32 @@
 <template>
     <div class="p-item-use">
         <span class="c-actionBtn" v-on:click="clickBtn">{{actionName}}</span>
-        <span class="c-need-item u-has-icon" v-for="(item,index) in useItems" :key="index" :class="iconClass(item.brID)">{{item.num}}</span>
+        <span class="c-need-item u-has-icon" v-for="(item,index) in useItems" :key="index" :class="items(item)" >{{item.num}}</span>
     </div>
 </template>
 
 <script>
-import {Resource} from '~/assets/class/basicResource';
+import {Resource,GlobalResource,LocalResource} from '~/assets/class/basicResource';
 export default {
     name:'resourceUseBtn',
     props:['actionName','useItems','clickMethod','actionAgument'],
     computed:{
         items(){
-            let store = this.$store;
-            return this.useItems.map(obj=>{
-                Resource.getIconClass(obj.brID);
-            })
-        }
+            return OBJ =>{
+                let store = this.$store;
+                let icon = Resource.getIconClass(OBJ.brID);
+
+                let isEnough = (obj) =>{
+                    if(Resource.isGlobalResource(obj.brID)){
+                        return store.getters['gameOperator/isResourceEnoughInGlobal'](obj.brID,obj.num);
+                    }else{
+                        let cityID = store.state.gameOperator.selectedCityID;
+                        return store.getters['city/isResourceEnoughInCity'](obj.brID,cityID,obj.num);
+                    }
+                }
+                return [icon,isEnough(OBJ)]
+            }
+        },
     },
     methods:{
         clickBtn(){
